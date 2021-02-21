b043c4c91abcb0aa104dba2512d47fb5d4975255
owel <rwlllaguitan@yahoo.com>
2021-02-21 21:25:32 +0800

    reads excel workbook and prints

--- src/main/java/Fundamentals/FileReader/ReadingExcel.java
+++ src/main/java/Fundamentals/FileReader/ReadingExcel.java
@@ -1,5 +1,11 @@
 package Fundamentals.FileReader;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -14,7 +20,32 @@ public class ReadingExcel {
         FileInputStream fileInputStream = new FileInputStream(excelFilePath);
 
         //OBJECT VARIABLE FOR THE WORKBOOK
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
 
+        //creating sheet object to retrieve object
+        XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(0);
+
+        //evaluating cell type
+        FormulaEvaluator formulaEvaluator = xssfWorkbook.getCreationHelper().createFormulaEvaluator();
+
+        //iteration over row
+        for(Row row : xssfSheet){
+            //iteration over cell
+            for (Cell cell : row){
+                switch (formulaEvaluator.evaluateInCell(cell).getCellType()){
+                    //getting value of numeric
+                    case NUMERIC:
+                        System.out.println(cell.getNumericCellValue()+ "\t\t");
+                        break;
+                    //getting value of string
+                    case STRING:
+                        System.out.println(cell.getStringCellValue()+ "\t\t");
+                        break;
+                }
+            }
+            System.out.println();
+
+        }
 
 
 
