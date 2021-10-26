eaceb45e88226a4f3df84cef907093b43bd386ac
Work <rwlllaguitan@yahoo.com>
2021-10-26 17:41:34 +0800

    nested loops lessons

--- src/main/java/Fundamentals/Loop/ForEachArray2Dimen2.java
+++ src/main/java/Fundamentals/Loop/ForEachArray2Dimen2.java
@@ -3,7 +3,7 @@ package Fundamentals.Loop;
 /*
  *Created by Work on 26/10/2021 4:47 PM
  */
-public class Print2DArray {
+public class ForEachArray2Dimen2 {
     public static void main(String[] args) {
 
         int twoDimen [][] = {
---
+++ src/main/java/Fundamentals/Loop/Nestedloop2.java
@@ -0,0 +1,34 @@
+package Fundamentals.Loop;
+
+/*
+ *Created by Work on 26/10/2021 5:28 PM
+ * inverted triangles
+ */
+public class Nestedloop2 {
+    public static void main(String[] args) {
+
+        int k = 1;
+        for(int i = 0 ; i < 4 ; i++){
+
+            for (int j = 0 ; j < 4-i ; j++){
+                System.out.print(" "+k);
+                k++;
+            }
+            System.out.println("");
+        }
+
+        System.out.println("-----------------------");
+
+        int a = 1;
+        for(int b = 0 ; b < 4 ; b++){
+
+            for (int c = 0 ; c < b+1 ; c++){
+                System.out.print(" "+a);
+                a++;
+            }
+            System.out.println("");
+        }
+
+
+    }
+}
